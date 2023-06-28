250633ccd121c556cfa300ae2521417bcb111bbe
Peter Kyale <kyalepeter2000@gmail.com>
2023-06-28 11:06:16 +0300

    Modified the Codex function to accept int inputs and add them to a predefined value to produce an output

--- codex_api.py
+++ codex_api.py
@@ -1,22 +1,46 @@
 import requests
 import gradio
 
-def codex(name):
-    url = 'https://api.codex.jaagrav.in'
-    headers = {
-        'Content-Type': 'application/x-www-form-urlencoded'
-    }
+# def codex(value):
+#     url = 'https://api.codex.jaagrav.in/execute'
+#     headers = {
+#         'Content-Type': 'application/json'
+#     }
+#     data = {
+#         'code': f"predefined_number = 5\nresult = predefined_number + {value}\nprint(result)",
+#         'language': 'python',
+#         'input': ''
+#     }
+
+#     response = requests.post(url, headers=headers, json=data)
+#     if response.ok:
+#         result = response.json()['stdout']
+#         return result
+#     else:
+#         return "Error: Failed to execute the code."
+
+# demo = gradio.Interface(fn=codex, inputs='number', outputs='text', title="Codex API: Add Predefined Number")
+# demo.launch()
+
+
+def CodexAPI():
+    value = int(input("Enter value: "))
     data = {
-        'code': "Good morning, {}".format(name),
-        'language': 'py',
-        'input': ''
+    'code': f'val = {value}  + 5\nprint(val)',
+    'language': 'py',
+    'input': ''
+}
+
+    headers = {
+    'Content-Type': 'application/x-www-form-urlencoded'
     }
 
-    response = requests.post(url, headers=headers, data=data)
-    # print(response.status_code)
-    # print(response.content)
-    result = response.json()['output']
-    return result
+    response = requests.post('https://api.codex.jaagrav.in', headers=headers, data=data)
 
-demo = gradio.Interface(fn=codex, inputs='text', outputs='text', title="Codex API for Python Compiler")
-demo.launch()
+    if response.ok:
+        print(response.json())
+    else:
+        print(response.text)
+
+CodexAPI()  
+
